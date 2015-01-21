47d05c8672b82b5c3034dbda8c172c084acb9f4b
Evgeniy Leybovich <evgeniy@linnovate.net>
2015-01-21 12:36:03 +0200

    facebook creds updated

--- config.js
+++ config.js
@@ -2,18 +2,18 @@ var config = {};
 
 // facebook app params
 config.fb_auth_params = {
-    appId : process.env['FACEBOOK_APPID'] || '593100544072637',
-    appSecret: process.env['FACEBOOK_SECRET'] || '1e383954a0e5ce39be394d1e4e967bc7',
-    appName: process.env['FACEBOOK_APPNAME'] || 'idemos_dev',
+    appId : process.env['FACEBOOK_APPID'] || '352750874913187',
+    appSecret: process.env['FACEBOOK_SECRET'] || '8f25ee5ffe420955656613f6ef8da91b',
+    appName: process.env['FACEBOOK_APPNAME'] || 'DemocraSee',
     callback: config.ROOT_PATH + '/account/facebooklogin',
     scope: 'email,publish_actions',
     failedUri: '/noauth'
 };
 
 config.fb_general_params = {
-    fb_title: 'עורו',
-    fb_description:  "עורו היא תנועה חברתית לייצוג הרוב בישראל. אנו מאמינים שבעידן שבו אנו חיים, כולנו מסוגלים וזכאים להשתתף בקבלת ההחלטות. לכן, עורו מנהלת פלטפורמה לדיון ציבורי, יסודי ואפקטיבי שיוביל שינוי בסדר היום. אצלנו, האג'נדה מוכתבת מלמטה.",
-    fb_image: 'http://site.e-dologic.co.il/philip_morris/Xls_script/uru_mailing/logo.jpg'
+    fb_title: '',
+    fb_description:  "",
+    fb_image: ''
 }
 
 // amazon s3 credentials
