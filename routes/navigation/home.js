0373ae0c716b4c36c33d1b654a32a7e80086c477
Evgeniy Leybovich <evgeniy@linnovate.net>
2015-02-03 17:17:57 +0200

    redirects from home

--- routes/navigation/home.js
+++ routes/navigation/home.js
@@ -1,3 +1,8 @@
 module.exports = function(req, res){
-     res.render('index.ejs');
+        //if (signUpSuccessful(request, response)) {
+    res.statusCode = 302;
+    res.setHeader("Location", "/discussions/549ab39ada8a349714000026");
+    res.end();
+        //}
+     //res.render('index.ejs');
 }
