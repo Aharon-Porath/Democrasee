4523acb93eb19bffcc83d0145c8d3e1d5b65a8f8
Evgeniy Leybovich <evgeniy@linnovate.net>
2015-02-08 12:38:45 +0200

    tlv medo wrapper in iframe

---
+++ tlv/index_files/FullScreenResize.js
@@ -0,0 +1,65 @@
+﻿var rtime = new Date(1, 1, 2000, 12, 00, 00);
+var timeout = false;
+
+var FullScreenResize = {
+       
+    delta: 0,// 200,
+    containerWidth: 1214,
+    containerHeight: 800,
+    maxBgHeight: 800,
+    topDelta: 0,
+    adminMenuHeight: 0,
+
+    resizeend: function () {
+        if (new Date() - rtime < this.delta) {
+            setTimeout(this.resizeend, this.delta);
+        } else {
+            timeout = false;
+            FullScreenResize.setBackgroundHeight();
+            FullScreenResize.setBackgroundWidth();
+        }
+    },
+
+    setBackgroundHeight: function() {
+        var windowHeight = $(window).height();
+        var bg_height = this.maxBgHeight;
+        var bg_top = (bg_height - windowHeight) / 2;
+        //console.log("windowHeight: " + windowHeight + " bg_top: " + bg_top + " adminMenuHeight: " + this.adminMenuHeight);        
+        bg_top = windowHeight < this.containerHeight ? this.topDelta : bg_top;
+        bg_top -= this.adminMenuHeight;
+        bg_top = bg_top < 0 ? 0 : bg_top;
+        $('.bgpos').css("background-position", "center -" + bg_top + "px");
+        $("#container").css("top", "-" + bg_top + "px");
+    },
+
+    setBackgroundWidth: function() {
+        var windowWidth = $(window).width();
+        var bg_width = this.containerWidth;
+        var bg_left = (bg_width - windowWidth) / 2;
+        if (bg_left > 0) {
+            $("#container").css("left", "-" + bg_left + "px");
+        }
+        else {
+            $("#container").css("left", "0");
+        }
+    },
+
+    log: function (msg) { try { console.log(msg); } catch (e) { } },
+    end: function () { }
+};
+
+$(document).ready(function () {
+
+    $(".bodyContent").addClass("bgpos");
+    FullScreenResize.setBackgroundHeight();
+    FullScreenResize.setBackgroundWidth();   
+
+    $(window).resize(function () {
+        rtime = new Date();
+        if (timeout === false) {
+            timeout = true;
+            setTimeout(FullScreenResize.resizeend, FullScreenResize.delta);
+        }               
+    });
+
+});
---
+++ tlv/index_files/site.js
@@ -0,0 +1,36 @@
+﻿/// <reference path="jquery-1.8.2.intellisense.js" />
+$(document).ready(function () {
+    /* Menu */
+    $(".iMenu").hover(
+      function () {
+          var cssClass = $(this).attr("cssClass");
+          $(this).addClass("top");
+          $("#menu-text-hover").addClass(cssClass).show();
+      }, function () {
+          $("#menu-text-hover").removeAttr("class").hide();
+          $(this).removeClass("top");
+      }
+    );
+
+    /* Gallery */    
+    $("#arrow-right").click(function () {
+        galleryNext("right");
+    });
+    $("#arrow-left").click(function () {
+        galleryPrev("left");
+    });    
+});
+
+/* Gallery */
+function galleryNext(dir) {
+    var $display = $("#gallery img:visible");
+    var $next = $display.hide().next();
+    if ($next.length > 0) { $next.show('slide', { direction: dir }, 500); }
+    else { $("#gallery img:first").show('slide', { direction: dir }, 500); }
+}
+function galleryPrev(dir) {
+    var $display = $("#gallery img:visible");
+    var $prev = $display.hide().prev();
+    if ($prev.length > 0) { $prev.show('slide', { direction: dir }, 500); }
+    else { $("#gallery img:last").show('slide', { direction: dir }, 500); }
+}
