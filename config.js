380ee0d5d4f1d938dfd351f5beb7881a2dcd416a
ChavaG <chava@linnovate.net>
2015-05-04 11:36:16 +0300

    check existing system

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ config.s3_creds = {
 
 config.sendgrid_user = 'shailinnovate';
 config.sendgrid_key = 'Zaq1Xsw2';
-config.system_email = 'admin@dev6.linnovate.net';
+config.system_email = ' אמנת הולכי רגל ורוכבי אופניים  <democracy@linnovate.net> ';
 
 
 config.MAIN_DISCUSSION = process.env['MAIN_DISCUSSION'] || '549ab39ada8a349714000026';
