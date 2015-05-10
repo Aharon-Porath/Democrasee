25d4b2469632344ce9a797abe796f8e03c6d5985
ChavaG <chava@linnovate.net>
2015-05-10 10:39:32 +0300

    add notification to comment on suggestion

--- api/discussions/post_on_suggestion_resource.js
+++ api/discussions/post_on_suggestion_resource.js
@@ -2,13 +2,13 @@
 var resources = require('jest'),
     og_action = require('../../og/og.js').doAction,
     models = require('../../models'),
+    notifications = require('../notifications'),
     common = require('../common.js'),
     async = require('async'),
     _ = require('underscore');
 
 var PostOnSuggestionResource = module.exports = common.GamificationMongooseResource.extend({
     init:function () {
-
         this._super(models.PostSuggestion, null, 0);
         this.allowed_methods = ['get', 'post', 'delete'];
         this.authorization = new common.TokenAuthorization();
@@ -56,12 +56,73 @@ var PostOnSuggestionResource = module.exports = common.GamificationMongooseResou
     },
 
     create_obj: function(req, fields, callback) {
-        var self = this;
-        var user = req.session.user;
 
-        fields.creator_id = req.session.user.id;
+        var user_id = req.user.id;
+        var suggestion_creator_id = fields.suggestion_creator_id;
+
+        var discussion_id = fields.discussion_id;
+        var suggestion_id = fields.suggestion_id;
+
+        var self = this;
+        var user = req.user;
+
+        fields.creator_id = user.id;
         fields.first_name = user.first_name;
         fields.last_name = user.last_name;
+
+        var iterator = function (unique_user, itr_cbk) {
+            //if (unique_user  === req.session.user.id || !unique_user || unique_user === "undefined"){
+                console.log("user should not get mail if he is the notificator");
+                itr_cbk(null, 0);
+            //} else {
+            //    if (suggestion_creator_id === unique_user) {
+            //        notifications.create_user_notification("comment_on_discussion_change_suggestion_you_created", suggestion_id, unique_user, user_id, discussion_id, '/discussions/' + discussion_id, function (err, results) {
+            //            itr_cbk(err, results);
+            //        });
+            //    } else {
+            //        notifications.create_user_notification("comment_on_discussion_change_suggestion", suggestion_id, unique_user, user_id, discussion_id, '/discussions/' + discussion_id, function (err, results) {
+            //            itr_cbk(err, results);
+            //        });
+            //    }
+            //}
+        };
+
+        async.waterfall([
+            function (cbk) {
+                models.Discussion.findById(fields.discussion_id, cbk);
+            },
+            function (disc_obj, cbk){
+                // find all users that has this discussion in their discussion list (for notifications)
+                models.User.find({'discussions.discussion_id': discussion_id}, function(err, users){
+                    cbk(err, disc_obj, users);
+                });
+            },
+            //add notification for the dicussion's participants or creator
+            function (disc_obj, users, cbk) {
+
+                cbk();
+
+                var unique_users = [];
+
+                // be sure that there are no duplicated users in discussion.users
+                _.each(disc_obj.users, function (user) {
+                    unique_users.push(user.id || user.user_id + "")
+                });
+                _.each(users, function (user) {
+                    unique_users.push(user.id)
+                });
+                unique_users = _.uniq(unique_users);
+
+                async.forEach(unique_users, iterator, function(err){
+                    if(err){
+                        console.error(err);
+                        err.trace();
+                    }
+                });
+
+            }
+
+        ]);
 
         self._super(req, fields, function(err, post_suggestion){
             post_suggestion.avatar = req.user.avatar_url();
@@ -69,6 +130,7 @@ var PostOnSuggestionResource = module.exports = common.GamificationMongooseResou
 
             callback(err, post_suggestion);
         });
+
     },
 
     delete_obj: function(req,object,callback){
