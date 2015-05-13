2db9c58bcb2b3c07f5a6437a42981c5227271d6c
ChavaG <chava@linnovate.net>
2015-05-13 09:38:57 +0300

    mail links to iframe

--- config.js
+++ config.js
@@ -33,6 +33,6 @@ config.MAIN_DISCUSSION = process.env['MAIN_DISCUSSION'] || '549ab39ada8a34971400
 
 config.DB_URL = process.env['MONGOLAB_URI'] || 'mongodb://localhost/idemos';
 config.ROOT_PATH = process.env.ROOT_PATH || 'http://localhost:1180';
-config.INDEX_PATH = process.env.INDEX_PATH || 'http://localhost:1180';
+config.INDEX_PATH = process.env.INDEX_PATH || 'http://localhost:1180/tlv/index.html';
 
 module.exports = config;
